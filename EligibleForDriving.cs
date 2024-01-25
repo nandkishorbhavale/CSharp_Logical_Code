27bff7b68c98dad0816897886e4550226e9de005
Nandkishor <nandkishor.bhavale@dalisoft.in>
2024-01-25 15:30:43 +0530

    Add project files.

---
+++ EligibleForDriving.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Learning
+{
+    internal class EligibleForDriving
+    {
+        public static void Main1(string[] args) {
+            int age = 22;
+            if (age >= 18)
+            {
+                Console.WriteLine("You can Drive!");
+            }
+            else
+            {
+                Console.WriteLine("You cannot Drive, Sorry");
+            }
+        }
+    }
+}
---
+++ HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Program
+{
+    class HelloWorld
+    {
+        public static void Main(String[] args)
+        {
+            Console.Write("Hello Nandkishor bro");
+            Console.Write(" Hii, It's time to Change yourself");
+        }
+    }
+}
