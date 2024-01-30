027caaa8d9eafecde880c1397f0f15e17d59aecb
Nandkishor <nandkishor.bhavale@dalisoft.in>
2024-01-30 22:38:38 +0530

    OddEven

--- EligibleForDriving.cs
+++ EligibleForDriving.cs
@@ -8,16 +8,36 @@ namespace CSharp_Learning
 {
     internal class EligibleForDriving
     {
-        public static void Main1(string[] args) {
-            int age = 22;
-            if (age >= 18)
+        public static void Main(string[] args) {
+            DrivingAge();
+            oddEven();
+        }
+        public static void DrivingAge()
+        {
+            int age = 16;
+            if (age >= 18) 
             {
-                Console.WriteLine("You can Drive!");
+                Console.WriteLine("You you drive Bro!");
             }
             else
             {
-                Console.WriteLine("You cannot Drive, Sorry");
+                Console.WriteLine("Sorry Bro you cannot drive!");
             }
+        }
+        static void oddEven()
+        {
+            int number = 55;
+            if(number%2==0)
+            {
+                //bool even = true;
+                Console.WriteLine(number +" is Even Number.");
+            }
+            else
+            {
+               //bool odd =false;
+                Console.WriteLine(number + " is Odd Number.");
+            }
+           
         }
     }
 }
--- HelloWorld.cs
+++ HelloWorld.cs
@@ -3,7 +3,7 @@ namespace Program
 {
     class HelloWorld
     {
-        public static void Main(String[] args)
+        public static void Main1(String[] args)
         {
             Console.Write("Hello Nandkishor bro");
             Console.Write(" Hii, It's time to Change yourself");
